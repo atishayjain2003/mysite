125b637a94275366415044b47a64377d15a805ec
atishayjain2003 <atishayjain2003@gmail.com>
2024-06-09 02:42:30 +0530

    Working with models

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class Authoradmin(admin.ModelAdmin):
+    list_display=('title','slug', 'author')
+admin.site.register(models.Post, Authoradmin)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,2 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Post(models.Model):
+    options=(('draft','Draft'),
+             ('published','Published'),
+             )
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250, unique_for_date='publish')
+    publish=models.DateTimeField(default=timezone.now)
+    author=models.ForeignKey(User,on_delete=models.CASCADE, related_name='blog_posts')
+    #if a user is deleted then all the post related to that user will be deleted 
+    # this is the function of delete=models.cascade
+    content=models.TextField(default=' ')
+    status=models.CharField(max_length=10, choices=options, default='draft')
+    #makemigrations is used to prepare the model to make changes to the database
+    #migrate is used to make the changes to the database
+
+    class Meta:
+        ordering = ('-publish',)
+    # for ordering the blogs by date
+    
+    def __str__(self):
+        return self.title
+    #used to display title on django admin site
 
